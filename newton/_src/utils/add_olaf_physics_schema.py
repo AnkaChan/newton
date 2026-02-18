8a963405629e655ed9ecb5feaad4265ac94c0378
apradhana <andre.pradhana@gmail.com>
2026-02-17 23:01:49 -0800

    Add add_olaf_physics_schema.py.

---
+++ newton/_src/utils/add_olaf_physics_schema.py
@@ -0,0 +1,28 @@
+import argparse
+
+import numpy as np
+import warp as wp
+from pxr import Gf, Usd, UsdGeom, UsdPhysics
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_path", type=str)
+    args = parser.parse_args()
+
+    output_path = args.input_path.replace(".usd", "_physics.usd")
+
+    stage = Usd.Stage.Open(args.input_path)
+
+    for prim in stage.Traverse():
+        if "cobble" in str(prim.GetPath()):
+            continue
+        path = str(prim.GetPath()).split("/")
+
+        # ROBOT
+        if any(name in path[-1] for name in ("PELVIS", "HIP", "KNEE", "ANKLE", "FOOT", "NECK", "SHOULDER", "ARM", "JAW", "BROW", "EYE", "HEAD")):
+            print(f"Applying RigidBodyAPI to {prim}")
+            rigidBodyAPI = UsdPhysics.RigidBodyAPI.Apply(prim)
+            rigidBodyAPI.CreateKinematicEnabledAttr(True)
+
+    print(f"Saving to {output_path}")
+    stage.Export(output_path)
