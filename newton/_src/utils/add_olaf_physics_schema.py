3043d9bd5b31e8f15e53d580ea4f7fcf7b3aa436
apradhana <andre.pradhana@gmail.com>
2026-02-25 21:26:25 -0800

    Add collision schema to RBD (stoolWoodB) and plane.

--- newton/_src/utils/add_olaf_physics_schema.py
+++ newton/_src/utils/add_olaf_physics_schema.py
@@ -30,14 +30,28 @@ if __name__ == "__main__":
         path = str(prim.GetPath()).split("/")
 
         # ROBOT
-        # if any(name in path[-1] for name in ("PELVIS", "HIP", "KNEE", "ANKLE", "FOOT", "NECK", "SHOULDER", "ARM", "JAW", "BROW", "EYE", "HEAD")):
-        if any(name in path[-1] for name in ("FOOT", "ANKLE", "PELVIS")):
+        if any(name in path[-1] for name in ("PELVIS", "HIP", "KNEE", "ANKLE", "FOOT", "NECK", "SHOULDER", "ARM", "JAW", "BROW", "EYE", "HEAD")):
+        # if any(name in path[-1] for name in ("FOOT", "ANKLE", "PELVIS")):
             print(f"Applying RigidBodyAPI to {prim}")
             rigidBodyAPI = UsdPhysics.RigidBodyAPI.Apply(prim)
             rigidBodyAPI.CreateKinematicEnabledAttr(True)
 
             for child in prim.GetChildren():
                 apply_collision_api(child)
+
+        # TERRAIN (adjust)
+        elif any(name in path[-1] for name in ("Plane",)):
+            print(f"Applying CollisionAPI to {prim}")
+            collisionAPI = UsdPhysics.CollisionAPI.Apply(prim)
+            collisionAPI.CreateCollisionEnabledAttr(True)
+
+        # RIGID BODIES (adjust)
+        elif len(path) == 5 and any(name in path[-1] for name in ("stoolWoodB", )):
+            print(f"prim.GetPath(): {str(prim.GetPath())}")
+            print(f"Applying RigidBodyAPI and MassAPI to {prim}")
+            rigidBodyAPI = UsdPhysics.RigidBodyAPI.Apply(prim)
+            massAPI = UsdPhysics.MassAPI.Apply(prim)
+            apply_collision_api(prim)
 
     print(f"Saving to {output_path}")
     stage.Export(output_path)
